26e74a03ebcd33783295c579885eaaa7a4f4ced2
Jake Devery <S00234729@atu.ie>
2023-09-25 11:17:22 +0100

    Completed ex 2 and used a for each loop

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Ex2
 {
@@ -10,6 +12,53 @@ namespace Ex2
     {
         static void Main(string[] args)
         {
+            //read file
+            string filePath = @"C:\Users\S00234729\Downloads\results.txt";
+            try
+            {
+
+                string[] fileContents = File.ReadAllLines(filePath);
+
+                //calc total
+                int totalPoints = 0, points = 0, result = 0;
+
+                foreach (string line in fileContents) 
+                {
+                    result = Convert.ToInt32(line);
+
+                    if (result >= 90)
+                        points = 100;
+                    else if (result >= 80)
+                        points = 88;
+                    else if (result >= 70)
+                        points = 77;
+                    else if (result >= 60)
+                        points = 66;
+                    else if (result >= 50)
+                        points = 56;
+                    else if (result >= 40)
+                        points = 46;
+                    else if (result >= 30)
+                        points = 37;
+                    else
+                        points = 0;
+
+                    totalPoints += points;
+                }
+
+                // amend file
+
+                File.AppendAllText(filePath, Environment.NewLine + "Total Points: " + totalPoints.ToString());
+
+
+
+            }
+
+
+            catch (IOException io)
+            {
+                Console.WriteLine(io.Message);
+            }
         }
     }
 }
